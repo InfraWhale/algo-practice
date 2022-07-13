43af01455bf9fc6507251964f29cdadaa7271ba5
InfraWhale <gyqjajang@gmail.com>
2022-07-13 14:33:04 +0900

    Insertion Sort

---
+++ src/chap04/Multiplication.java
@@ -0,0 +1,19 @@
+package chap04;
+
+public class Multiplication {
+
+  public static void main(String[] args) {
+    int step, num;
+
+    for (step = 1; step <= 9; step++) {
+      System.out.printf("%d단", step);
+      for (num = 1; num <= 9 ; num++) {
+        System.out.printf("\t%2d", step*num);
+      }
+
+      System.out.print("\n");
+    }
+
+  }
+
+}
