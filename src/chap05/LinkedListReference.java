884690f6b74b351c77b238ca4db5ed7e9d49f239
InfraWhale <gyqjajang@gmail.com>
2022-07-14 23:24:09 +0900

    BinarySearchTree

--- src/chap05/LinkedListReference.java
+++ src/chap05/LinkedListReference.java
@@ -17,7 +17,7 @@ public class LinkedListReference {
   public LinkedListReference() {
   }
 
-  public String get(int index) {
+  public String get(int index) { // 인덱스 번호에 입력된 이름 반환하는 메소드
     if (head == null) {
       return null;
     }
@@ -32,7 +32,7 @@ public class LinkedListReference {
     return cur.name;
   }
 
-  public void insHead(String val) {
+  public void insHead(String val) { // head 위치 바꾸는 메소드
     if (head == null) {
       head = new StationList(val);
       return;
@@ -43,7 +43,7 @@ public class LinkedListReference {
     head = node;
   }
 
-  public void insTail(String val) {
+  public void insTail(String val) { // 연결 리스트 마지막에 요소 삽입 (tail 위치 변경)
     if (head == null) {
       head = new StationList(val);
       return;
@@ -89,8 +89,8 @@ public class LinkedListReference {
   }
 
   public void insIndex2(int insIndex, String val, int prevInd) {
-    if (head == null) {
-      if (insIndex == 0) {
+    if (head == null) { // head에 값이 없으면 끝
+      if (insIndex == 0) { // 근데 삽입하는 index가 0이면 head에 이거 삽입
         head = new StationList(val);
       }
       return;
@@ -98,7 +98,7 @@ public class LinkedListReference {
 
     StationList prev = null;
     StationList cur = head;
-    for (int i = 0; i < prevInd; i++) {
+    for (int i = 0; i < prevInd; i++) { // 넣으려는 위치 prevInd로 설정
       if (cur == null) {
         return;
       }
@@ -120,13 +120,13 @@ public class LinkedListReference {
   }
 
   public void delIndex(int index) {
-    if (head == null) {
+    if (head == null) { // 아무 요소도 없으면 끝냄
       return;
     }
 
     StationList prev = null;
     StationList cur = head;
-    for (int i = 0; i < index; i++) {
+    for (int i = 0; i < index; i++) { // 삭제할 요소를 cur로 설정
       if (cur.next == null) {
         return;
       }
@@ -135,11 +135,11 @@ public class LinkedListReference {
     }
 
     StationList next = cur.next;
-    if (prev == null) {
+    if (prev == null) { // cur이 head인 경우
       head = head.next;
-    } else if (next == null) {
+    } else if (next == null) { // 마지막 요소인 경우
       prev.next = null;
-    } else {
+    } else { // 그 외
       prev.next = next;
     }
   }
@@ -151,7 +151,7 @@ public class LinkedListReference {
       System.out.print(point.name + " → ");
       point = point.next;
     }
-    System.out.println(point.name + " → ");
+    System.out.println(point.name);
   }
 
   public static void main(String[] args) {
@@ -162,6 +162,7 @@ public class LinkedListReference {
     obj.insTail("[동대구]");
     obj.insTail("[부산]");
     obj.retrieve();
+    // obj.insIndex1(1, "[광명]");
     obj.insIndex2(4, "[광명]", 1);
     obj.retrieve();
     obj.delIndex(1);
