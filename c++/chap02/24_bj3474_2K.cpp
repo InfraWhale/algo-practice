4648afb775dcc0413fff0fe88d1cd2eff20a78c1
InfraWhale <gyqjajang@gmail.com>
2024-03-25 12:26:45 +0900

    bj 10709 answer

---
+++ c++/chap02/24_bj3474_2K.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> // 백준 3474 2K
+using namespace std;
+
+int n, m, a[104][104];
+string s;
+
+int main(){
+	
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < m; j++){
+			if(s[j] == '.')a[i][j] = -1;
+			else a[i][j] = 0;
+		}
+	}
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++){
+			if(a[i][j] == 0) {
+				int cnt = 1;
+				while(a[i][j + 1] == -1){
+					a[i][j + 1] = cnt++;
+					j++;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
