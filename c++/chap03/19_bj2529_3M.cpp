78b68b0cfeb9e0dc258af778a26b377c59a726ce
InfraWhale <gyqjajang@gmail.com>
2024-04-29 00:21:30 +0900

    bj 2529

--- c++/chap03/19_bj2529_3M.cpp
+++ c++/chap03/19_bj2529_3M.cpp
@@ -1,37 +1,28 @@
 #include<bits/stdc++.h> // 백준 2529 3M
 using namespace std;
 
-int k, a[10], maxStart, minStart;
+int k, a[14];
 vector<char> vc;
 vector<int> vMax, vMin;
 bool flagMax, flagMin;
 
 bool bs(char c, int a, int b) {
-	if(c == '<') {
-		if(a > b) {
-			return 0;
-		} else {
-			return 1;
-		}
-	} else {
-		if(a > b) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
+	if(c == '<') return a < b;
+	else return a > b;
 }
 
 void getMax(int depth) {
-	if(depth == k + 1) {
+	if(depth == k) {
 		flagMax = 1;
 		return;
 	}
 	for (int i = 9; i >= 0; i--) {
 		if(!a[i] && bs(vc[depth], vMax[depth], i)) {
 			a[i] = 1;
-			vMax.push_back(i);
+			vMax.push_back(i);	
+			// cout << "+ " << i << "\n";		
 			getMax(depth+1);
+			if(flagMax) return;
 			a[i] = 0;
 			vMax.pop_back();
 		}
@@ -39,7 +30,7 @@ void getMax(int depth) {
 }
 
 void getMin(int depth) {
-	if(depth == k + 1) {
+	if(depth == k) {
 		flagMin = 1;
 		return;
 	}
@@ -47,7 +38,9 @@ void getMin(int depth) {
 		if(!a[i] && bs(vc[depth], vMin[depth], i)) {
 			a[i] = 1;
 			vMin.push_back(i);
+			// cout << "+ " << i << "\n";	
 			getMin(depth+1);
+			if(flagMin) return;
 			a[i] = 0;
 			vMin.pop_back();
 		}
@@ -60,22 +53,30 @@ int main() {
 	cout.tie(NULL); 
 	cin >> k;
 	for (int i = 0; i < k; i++) {
-		cin >> vc[i];
+		char temp;
+		cin >> temp;
+		vc.push_back(temp);
 	}
 
+	fill(&a[0], &a[0] + 10, 0);
+
 	for (int i = 9; i >= 0; i--) {
-		vMax[0] = i;
+		vMax.push_back(i); // 벡터에 i를 할당하기 전에 =를 써서 값을 넣으면 오류 발생 (vMax[0] = i 안됨)
 		a[i] = 1;
 		getMax(0);
 		if(flagMax) break;
+		vMax.clear();
 		a[i] = 0;
 	}
 
+	fill(&a[0], &a[0] + 10, 0);
+
 	for (int i = 0; i <= 9; i++) {
-		vMin[0] = i;
+		vMin.push_back(i);
 		a[i] = 1;
 		getMin(0);
 		if(flagMin) break;
+		vMin.clear();
 		a[i] = 0;
 	}
 
