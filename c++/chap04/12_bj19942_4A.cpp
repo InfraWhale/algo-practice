98f3344b75bb9162d4cf775111102d725333940d
InfraWhale <gyqjajang@gmail.com>
2024-05-23 17:52:44 +0900

    bj 19942

--- c++/chap04/12_bj19942_4A.cpp
+++ c++/chap04/12_bj19942_4A.cpp
@@ -1,7 +1,11 @@
-#include<bits/stdc++.h> // 백준 19942 4A
+#include<bits/stdc++.h> // 백준 19942 4A (사전순에서 틀리는거 같은데 모르겠음)
 using namespace std;
 
+// 실수로 하는 변수중복은 피하자...!!!
+
 int n, mp, mf, ms, mv, p[20], f[20], s[20], v[20], c[20], ret = 987654321, bit;
+bool flag;
+vector<vector<int>> vv;
 
 int main() { 
 	cin >> n;
@@ -13,30 +17,42 @@ int main() {
 	
 	for(int i = 0; i < (1 << n); i++) {
 		int tp=0, tf=0, ts=0, tv=0, tc=0;
+		vector<int> vec;
 		for(int j = 0; j < n; j++) {
 			if(i & (1 << j)) {
+				vec.push_back(j + 1);
 				tp += p[j];
 				tf += f[j];
 				ts += s[j];
-				tv += s[j];
+				tv += v[j];
 				tc += c[j];
 			}
 		}
-		if(tp >= mp && tf >= mf && ts >= ms && tv >= mv && tc < ret) {
+		// cout << tp << " : "<< tf << " : "<< ts << " : "<< tv << " : "<< tc << "\n";
+		// cout << bit << "\n";
+		if(tp >= mp && tf >= mf && ts >= ms && tv >= mv) {
 			// cout << tp << " : "<< tf << " : "<< ts << " : "<< tv << " : "<< tc << "\n";
 			// cout << bit << "\n";
-			ret = tc;
-			bit = i;
+			if(tc < ret) {
+				vv.clear();
+				vv.push_back(vec);
+				ret = tc;
+			} else if (tc == ret) {
+				vv.push_back(vec);
+			}
 		}
 	}
+	if(ret != 987654321) {
+		cout << ret << "\n";
 
-	cout << ret << "\n";
-
-	for(int i = 0; i < n; i++) {
-		if(bit & (1 << i)) {
-			cout << i+1 << " ";
+		sort(vv.begin(), vv.end());
+		for(int i : vv[0]) {
+			cout << i << " ";
 		}
+	} else {
+		cout << -1;
 	}
+
     return 0;
 }
 
